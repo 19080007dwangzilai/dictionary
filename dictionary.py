7244a2a7e13a13a18e0efa127ebabbe501190bfb
wangzilai <19080007d@connect.polyu.hk>
2019-12-25 15:08:36 +0800

    refactor dictionary

--- dictionary.py
+++ dictionary.py
@@ -1,29 +1,47 @@
-data = []
-count = 0
-with open('reviews.txt', 'r')as f:
-    for line in f:
-        data.append(line.strip())
-        count += 1
-        if count % 1000 == 0:
-            print(len(data))
-wc = {}
-for d in data:
-    words = d.split(' ')
-    for word in words:
-        if word in wc:
-            wc[word] += 1
+def read_file(filename):
+    data = []
+    count = 0
+    with open(filename, 'r')as f:
+        for line in f:
+            data.append(line.strip())
+            count += 1
+            if count % 1000 == 0:
+                print(len(data))
+    return data
+
+
+def dic(data):
+    wc = {}
+    for d in data:
+        words = d.split(' ')
+        for word in words:
+            if word in wc:
+                wc[word] += 1
+            else:
+                wc[word] = 1
+    for word in wc:
+        if wc[word] > 1000000:
+            print(word, wc[word])
+    print(len(wc))
+    return wc
+
+
+def user_check(wc):
+    while True:
+        word = input('enter the key you want to check:')
+        if word == 'q':
+            break
         else:
-            wc[word] = 1
-for word in wc:
-    if wc[word] > 1000000:
-        print(word, wc[word])
-print(len(wc))
-while True:
-    word = input('enter the key you want to check:')
-    if word == 'q':
-        break
-    else:
-        if word in wc:
-            print(word,'出现的次数', wc[word])
-        else:
-            print('not found')
+            if word in wc:
+                print(word, '出现的次数', wc[word])
+            else:
+                print('not found')
+
+
+def main():
+    data = read_file('reviews.txt')
+    wc = dic(data)
+    user_check(wc)
+
+
+main()
